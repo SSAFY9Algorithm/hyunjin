617bda9377463ebbfa4bdf11a5f8da02cea5315b
hyundang <hyunjin5697@yonsei.ac.kr>
2023-02-17 17:14:53 +0900

    연산자 끼워넣기 수정

--- dfs_bfs/Main_연산자끼워넣기_14888_s1_136ms.java
+++ dfs_bfs/Main_연산자끼워넣기_14888_s1_136ms.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_연산자끼워넣기_14888_s1_148ms {
+public class Main_연산자끼워넣기_14888_s1_136ms {
 	static int[] opCnt = new int[4];
 	static int[] numList;
 	static int n, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
@@ -26,31 +26,14 @@ public class Main_연산자끼워넣기_14888_s1_148ms {
 			opCnt[i] = Integer.parseInt(st.nextToken());
 		}
 
-		findMax(0, numList[0]);
-		findMin(0, numList[0]);
+		find(0, numList[0]);
 		System.out.println(max);
 		System.out.println(min);
 	}
 
-	// 최소값 구할 때
-	public static void findMin(int depth, int res) {
+	public static void find(int depth, int res) {
 		if (depth == n - 1) {
 			min = Math.min(min, res);
-			return;
-		}
-
-		for (int i = 0; i < 4; i++) {
-			if (opCnt[i] == 0)
-				continue;
-			opCnt[i]--;
-			findMin(depth + 1, calc(res, i, numList[depth + 1]));
-			opCnt[i]++;
-		}
-	}
-
-	// 최대값 구할 때
-	public static void findMax(int depth, int res) {
-		if (depth == n - 1) {
 			max = Math.max(max, res);
 			return;
 		}
@@ -59,7 +42,7 @@ public class Main_연산자끼워넣기_14888_s1_148ms {
 			if (opCnt[i] == 0)
 				continue;
 			opCnt[i]--;
-			findMax(depth + 1, calc(res, i, numList[depth + 1]));
+			find(depth + 1, calc(res, i, numList[depth + 1]));
 			opCnt[i]++;
 		}
 	}
